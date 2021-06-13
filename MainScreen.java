5989034bda73a05bc25296c8da622e80118a783a
M̸̢̠̘̣̦̩̦̮̰̤̅͘͝ạ̵̯̠̫͓̖̲͈̰̬̈̈͆̂̈́́̌͂̚d̸̝̰̬͇̝̲̬̪͊͂̀͂͗h̴̬͔͚͊͑͘͝ǘ̷̗̙͚̪̂̐̓̈́̄̄̑͜v̴̙̊̀̽̄̎̌̀̿͜͝ ̵̧̳̜̮̜̝̈́̈́͘S̵̩̻̜̍́̏̿h̸̪̲͍̺͍̠̀͊̑a̸̧̱̩̙͚͔̻̙̎̓̋̓͝r̴̢̢̛̻͗̎͗͑̊m̷̨̖͎̞͖͛͊̓̕à̷̧̨̨̙̮͕̫̟̲̇̔͒͗̇͊̉ <31816381+madhuv-sharma@users.noreply.github.com>
2021-06-13 21:52:33 +0530

    Update MainScreen.java

--- MainScreen.java
+++ MainScreen.java
@@ -5,13 +5,11 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.Scanner;
 
-public class MainScreen implements Serializable{
-	
-	
-
+public class MainScreen implements Serializable
+{
 	public static void main(String args[])
 	{
-		int nod,type,bookingno,ld,d;
+	    int nod,type,bookingno,ld,d;
 	    char ch='y';
 	    double ff;
 	    String name;
@@ -29,53 +27,39 @@ public class MainScreen implements Serializable{
 	    for(i=0;i<3;i++)
 	    {
 	    	ly[i]=new Luxury();
-			ly[i].set(500,false,false);
+		ly[i].set(500,false,false);
 	    }
 	    for(i=0;i<8;i++)
 	    {
 	    	dx[i]=new Deluxe();
-			dx[i].set(1500,true,false);
+		dx[i].set(1500,true,false);
 	    }
 	    for(i=0;i<10;i++)
 	    {
 	    	sdx[i]=new SuperDeluxe();
-			sdx[i].set(2500,true,false);
+		sdx[i].set(2500,true,false);
 	    }
 	    
 	    for(i=0;i<20;i++)
-	    {
 	    	c[i]=new Customer();
-	    }
 	    
 	    for(i=0;i<20;i++)
-	    {
 	    	b[i]=new Book();
-	    }
 	    
 	    for(i=0;i<20;i++)
-	    {
 	    	for(j=0;j<5;j++)
-	    	{
 	    		for(k=0;k<5;k++)
 	    		{
 	    			t[i][j][k]=new Transportation();
 	    			l[i][j][k]=new Laundry();
-	    			
 	    		}
-	    	}
-	    }
-	    
 	    
 	    String no;
 	    int ic=0,ily=0,ilr=0,itr1=0,itr2=0,isdx=0,idx=0;
-	    
 	    int flag1=0,flag2=0,flag3=0;
 
 	    while(true)
 	    {
-	    	
-//	    int is=0,js=0,ks=0;
-//	    int id=0,jd=0,kd=0;
 	    System.out.println("What do you want to do?");
 	    System.out.println("Book a room(b)");
 	    System.out.println("Avail a service(s)");
@@ -83,13 +67,12 @@ public class MainScreen implements Serializable{
 	    //System.out.println("Check Out(o)");
 	    //System.out.println("Display customer info based on booking number(r)");
 	    System.out.println("Exit Menu(e)");
-	    
+		    
 	    Scanner in = new Scanner(System.in);
-		ch = in.next(".").charAt(0);
+	    ch = in.next(".").charAt(0);
 		    
 	    if(ch=='b')
 	    {
-
 		    for(i=0;i<3;i++)
 		    {
 		    	if(ly[i].getStatus()==false)
@@ -99,9 +82,9 @@ public class MainScreen implements Serializable{
 		    		break;
 		    	}
 		    	else
-		    	flag1=0;
+			    	flag1=0;
 		    }
-		    
+    
 		    for(i=0;i<8;i++)
 		    {
 		    	if(dx[i].getStatus()==false)
@@ -111,7 +94,7 @@ public class MainScreen implements Serializable{
 		    		break;
 		    	}
 		    	else
-		    	flag2=0;
+			    	flag2=0;
 		    }
 		    
 		    for(i=0;i<10;i++)
@@ -123,56 +106,54 @@ public class MainScreen implements Serializable{
 		    		break;
 		    	}
 		    	else
-		    	flag3=0;
+			    	flag3=0;
 		    }
-	    	c[ic].setInitialDetails(ic);
-	    	b[ic].BookNew(c[ic],ly[ily],dx[idx],sdx[isdx],t,l,ily,flag1,idx,flag2,isdx,flag3);
-	    	c[ic].setBookingNo(b[ic].getBookingNumber());
-	        //System.out.println(ily);
-	    	ic++;
-
-
+		    c[ic].setInitialDetails(ic);
+		    b[ic].BookNew(c[ic],ly[ily],dx[idx],sdx[isdx],t,l,ily,flag1,idx,flag2,isdx,flag3);
+		    c[ic].setBookingNo(b[ic].getBookingNumber());
+		    //System.out.println(ily);
+		    ic++;
 	    }
 	    
 	    if(ch=='c')
         {
-            ic--;
-            Scanner in2 = new Scanner(System.in);
-            System.out.println("Enter your booking no");
-            no = in2.nextLine();
-           
-            int no1=(int)no.charAt(0)-48;
-            int no2;
-         
-            System.out.println("Enter type(1-lux,2-delux,3-super)");
-            no2= in2.nextInt();
-           
-            if(no2==1)
-            {
-                ly[no1].statuschange();
-               
+		    ic--;
+		    Scanner in2 = new Scanner(System.in);
+		    System.out.println("Enter your booking no");
+		    no = in2.nextLine();
+
+		    int no1=(int)no.charAt(0)-48;
+		    int no2;
+
+		    System.out.println("Enter type(1-lux,2-delux,3-super)");
+		    no2= in2.nextInt();
+
+		    if(no2==1)
+		    {
+			ly[no1].statuschange();
+
+		    }
+		    if(no2==2)
+		    {
+			dx[no1].statuschange();   
+
+		    }
+		    if(no2==3)
+		    {
+			sdx[no1].statuschange();
+		    }  
+
+		    System.out.println("Cancelled");
+
+		    //cancel in customer
             }
-            if(no2==2)
-            {
-                dx[no1].statuschange();   
-           
-            }
-            if(no2==3)
-            {
-                sdx[no1].statuschange();
-            }  
-           
-            System.out.println("Cancelled");
-           
-            //cancel in customer
-        }
-	    
+		    
 	    int cno;
-	    
+		    
 	    if(ch=='s')
 	    {
 	    	Scanner in2 = new Scanner(System.in);
-	  
+			
 	    	System.out.println("Enter your booking no");
 			no = in2.nextLine();
 			
@@ -184,47 +165,46 @@ public class MainScreen implements Serializable{
 	    	int a = in2.nextInt();
 		    
 	    	//itr1-> booking no
-		    itr1 = (int)(no.charAt(0))-48;	    
+			itr1 = (int)(no.charAt(0))-48;	    
 		    
-		    //itr2-> single or double
-		    if(no.charAt(1)=='s')
-		    itr2= (int)(no.charAt(4))-48;
-		    else 
-		    itr2= (int)(no.charAt(3))-48;
+			//itr2-> single or double
+			if(no.charAt(1)=='s')
+				itr2= (int)(no.charAt(4))-48;
+			else
+				itr2= (int)(no.charAt(3))-48;
 		    //better use some other conversion from integer to string
-		    
 		    //System.out.println(no+" "+a+" "+itr1+" "+itr2);
-		    if(a==1)
-		    {
-		    	
-		 		t[itr1][itr2][0].setDetails();
-		 		b[cno].s1=b[cno].s1+(t[itr1][itr2][0].getTotalCost());
-		 		b[cno].BookDisplay(b[cno].ff,c[cno].name,b[cno].s1,b[cno].s2,no);				
-		    }
-		    if(a==2)
-		    {
-		    	l[itr1][itr2][1].setDetails();
-		    	b[cno].s2=b[cno].s2+l[itr1][itr2][1].getTotalCost();
-		    	b[cno].BookDisplay(b[cno].ff,c[cno].name,b[cno].s1,b[cno].s2,no);
+			if(a==1)
+			{    	
+				t[itr1][itr2][0].setDetails();
+				b[cno].s1=b[cno].s1+(t[itr1][itr2][0].getTotalCost());
+				b[cno].BookDisplay(b[cno].ff,c[cno].name,b[cno].s1,b[cno].s2,no);				
+			}
+			if(a==2)
+			{
+				l[itr1][itr2][1].setDetails();
+				b[cno].s2=b[cno].s2+l[itr1][itr2][1].getTotalCost();
+				b[cno].BookDisplay(b[cno].ff,c[cno].name,b[cno].s1,b[cno].s2,no);
 			}
 	    }
 	    if(ch=='e')
-		    break;
+	    	break;
 	}
-	}
-	
-//	public void serializeBook(Book bc){
-//	 
-//		   try{
-//	 
-//			FileOutputStream fout = new FileOutputStream("e:\\address.ser");
-//			ObjectOutputStream oos = new ObjectOutputStream(fout);   
-//			oos.writeObject(bc);
-//			oos.close();
-//			System.out.println("Done");
-//	 
-//		   }catch(Exception ex){
-//			   ex.printStackTrace();
-//		   }
-//	   }
 }
+/*	
+public void serializeBook(Book bc)
+{
+		   try
+		   {
+			FileOutputStream fout = new FileOutputStream("e:\\address.ser");
+			ObjectOutputStream oos = new ObjectOutputStream(fout);   
+			oos.writeObject(bc);
+			oos.close();
+			System.out.println("Done");
+		   }
+		   catch(Exception ex)
+		   {
+			   ex.printStackTrace();
+		   }
+	   }
+}*/
